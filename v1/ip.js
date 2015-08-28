3338e6b8e67a9ad8d734860f5d722a035895797c
Zach <zbecker10@gmail.com>
2015-08-28 09:35:36 -0400

    ip.js

--- v1/ip.js
+++ v1/ip.js
@@ -1,24 +1,30 @@
-function fc_feature_ip() {
-    var base_url = "https://followercounter.github.io/static/v1";
-    var ip_address = document.createElement("a");
-    ip_address.href = "http://followercounter.co/";
-    ip_address.setAttribute("target", "_blank");
-    ip_address.className = "fc_feature_ip";
-    window.fc_feature_ip_callback = function(object) {
-        ip_address.innerHTML = object.ip;
-    };
-    var script = document.createElement("script");
-    script.async = true;
-    script.src = "https://api.ipify.org?format=jsonp&callback=window.fc_feature_ip_callback";
-    var script_index_zero = document.getElementsByTagName('script')[0];
-    script_index_zero.parentNode.insertBefore(script, script_index_zero);
-    var scripts = document.getElementsByTagName('script');
-    for (var i = 0; i < scripts.length; i++) {
-        if (scripts[i].src == base_url + '/ip.js') {
-            var script = scripts[i];
-            script.parentNode.insertBefore(ip_address, scripts[i]);
-            break
+if (!window.hasOwnProperty('followercounter')) {
+    window.followercounter = {};
+}
+window.followercounter.ip = {
+    link: null,
+    callback: function (object) {
+        this.link.innerHTML = object.ip;
+    },
+    init: function () {
+        var base_url = "http://localhost:8000/v1";
+        this.link = document.createElement("a");
+        this.link.href = "http://followercounter.co/";
+        this.link.setAttribute("target", "_blank");
+        this.link.className = "fc_feature_ip";
+        var script = document.createElement("script");
+        script.async = true;
+        script.src = "https://api.ipify.org?format=jsonp&callback=window.followercounter.ip.callback";
+        var script_index_zero = document.getElementsByTagName('script')[0];
+        script_index_zero.parentNode.insertBefore(script, script_index_zero);
+        var scripts = document.getElementsByTagName('script');
+        for (var i = 0; i < scripts.length; i++) {
+            if (scripts[i].src == base_url + '/ip.js') {
+                var script = scripts[i];
+                script.parentNode.insertBefore(window.followercounter.ip.link, scripts[i]);
+                break
+            }
         }
     }
 }
-fc_feature_ip();
+window.followercounter.ip.init();
