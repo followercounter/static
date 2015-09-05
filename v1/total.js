aec70a84ccde07a89516dfe080d24c4c514ce12a
Zach <zbecker10@gmail.com>
2015-09-05 01:19:35 -0400

    ["K", "M", "B", "T"]

--- v1/total.js
+++ v1/total.js
@@ -34,7 +34,7 @@ window.followercounter.total = function() {
     // }
     function m(number, decPlaces) {
     	decPlaces = Math.pow(10, decPlaces);
-    	var abbrev = ["k", "m", "b", "t"];
+    	var abbrev = ["K", "M", "B", "T"];
     	for (var i = abbrev.length - 1; i >= 0; i--) {
     		var size = Math.pow(10, (i + 1) * 3);
     		if (size <= number) {
