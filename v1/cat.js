33326ac2e88b2f928fd8f2c2f46e63b0f8f88850
Zach <zbecker10@gmail.com>
2015-09-28 01:48:54 -0400

    https

--- v1/cat.js
+++ v1/cat.js
@@ -6,7 +6,7 @@ if (!window.followercounter.hasOwnProperty('cat')) {
     window.followercounter.cat = function() {
         var wrap = document.createElement("div");
         var img = document.createElement("img");
-        img.src = "http://thecatapi.com/api/images/get?format=src&results_per_page=1&api_key=MzkxNDc?v=" + Math.random();
+        img.src = "https://thecatapi.com/api/images/get?format=src&results_per_page=1&api_key=MzkxNDc?v=" + Math.random();
         img.setAttribute("width", "100%");
         wrap.appendChild(img);
         document.write(wrap.innerHTML);
