d0857858bc585de3a91049aabfef0c39f4132b02
Zach <zbecker10@gmail.com>
2015-09-28 02:09:03 -0400

    link

--- v1/cat.js
+++ v1/cat.js
@@ -5,10 +5,15 @@ if (!window.hasOwnProperty('followercounter')) {
 if (!window.followercounter.hasOwnProperty('cat')) {
     window.followercounter.cat = function() {
         var wrap = document.createElement("div");
+        var link = document.createElement("a");
+        a.href = "https://github.com/followercounter/static/wiki/V1#random-cat";
+        a.setAttribute("target", "_blank");
         var img = document.createElement("img");
         img.src = "https://thecatapi.com/api/images/get?format=src&results_per_page=1&api_key=MzkxNDc?v=" + Math.random();
         img.setAttribute("width", "100%");
-        wrap.appendChild(img);
+        img.setAttribute("title", "Random Cat, by Follower Counter");
+        link.appendChild(img);
+        wrap.appendChild(link);
         document.write(wrap.innerHTML);
     }
 }
