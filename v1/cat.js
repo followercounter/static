fe8a04674dee78e584211d91bebc22097ce1094c
Zach <zbecker10@gmail.com>
2015-09-28 01:47:06 -0400

    cat :')

--- v1/cat.js
+++ v1/cat.js
@@ -2,13 +2,14 @@
 if (!window.hasOwnProperty('followercounter')) {
     window.followercounter = {};
 }
-
-window.followercounter.cat = function() {
-    var wrap = document.createElement("div");
-    var img = document.createElement("img");
-    img.src = "http://thecatapi.com/api/images/get?format=src&results_per_page=1&api_key=MzkxNDc";
-    img.setAttribute("width", "100%");
-    wrap.appendChild(img);
-    document.write(wrap.innerHTML);
+if (!window.followercounter.hasOwnProperty('cat')) {
+    window.followercounter.cat = function() {
+        var wrap = document.createElement("div");
+        var img = document.createElement("img");
+        img.src = "http://thecatapi.com/api/images/get?format=src&results_per_page=1&api_key=MzkxNDc?v=" + Math.random();
+        img.setAttribute("width", "100%");
+        wrap.appendChild(img);
+        document.write(wrap.innerHTML);
+    }
 }
 window.followercounter.cat();
